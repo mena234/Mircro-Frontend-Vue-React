2c247936deb9a044ba7b8689cb9dd733bcb857e7
Mena Nasser <menanasser234@outlook.sa>
2021-10-22 08:32:32 +0200

    Init project

---
+++ packages/vue-app/src/components/ReactComponentLoad.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <router-link to="/react/signup">Signup</router-link>
+    <router-link to="/react/signin">Signin</router-link>
+    <router-view></router-view>
+  </div>
+</template>
+<script>
+
+</script>
---
+++ packages/vue-app/src/components/Signin.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="container-center"> 
+     <img src="https://vuejs.org/images/logo.png" alt="VueJS logo" width="100" />
+    <h1>SignIn </h1>
+
+<router-link to="/signup">Signup</router-link>
+</div>
+</template>
+<script>
+
+export default {   
+}
+</script>
+<style scoped>
+.container-center{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+</style>
